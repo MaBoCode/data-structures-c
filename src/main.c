6ac8da1a8bb9c9b0a7b7d1de428101a27363eb95
Matthias Brown <8matthias.brown8@gmail.com>
2020-07-04 20:59:39 +0200

    [queue] Add tests

--- src/main.c
+++ src/main.c
@@ -4,17 +4,5 @@
 
 int main(void) {
 
-	q_queue *queue = q_create(3);
-
-	q_add(queue, 1);
-	q_add(queue, 2);
-	q_add(queue, 3);
-
-	q_print(queue);
-
-	int front = q_remove(queue);
-
-	printf("Front of queue is %d\n", front);
-
-	q_print(queue);
+	return 0;
 }
