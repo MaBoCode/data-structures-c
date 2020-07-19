f8105c0eb3ec76e2f18d25270d4bc892d5e10e77
Matthias Brown <8matthias.brown8@gmail.com>
2020-07-19 18:49:55 +0200

    [binary_tree] Add tests

---
+++ test/test_binary_tree.c
@@ -0,0 +1,23 @@
+#include "binary_tree.h"
+
+int main(void) {
+
+	bt_tree* tree = bt_create_tree();
+
+	bt_insert(tree, 5);
+	bt_insert(tree, 1);
+	bt_insert(tree, 0);
+	bt_insert(tree, 7);
+	bt_insert(tree, 6);
+
+	bt_print_inorder(tree->root);
+	printf("\n");
+
+	bt_print_preorder(tree->root);
+	printf("\n");
+
+	bt_print_postorder(tree->root);
+	printf("\n");
+
+	return 0;
+}
